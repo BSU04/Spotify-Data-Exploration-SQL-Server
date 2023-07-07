6bbe19e50396115518158bed437c55a0507e36f4
Coderaze07 <93376652+Coderaze07@users.noreply.github.com>
2023-07-07 23:02:52 +0530

    column exploration

--- spotify-data-exp.sql
+++ spotify-data-exp.sql
@@ -1,17 +1,18 @@
+/***Data Exploration of the Spotify Dataset Obtained from Kaggle***/
+
 -- display the table
 select * from dbo.spot
 
 
---count of rows 
+--total count of rows 
 select count(*) as
 Row_Count from dbo.spot
 
 
--- display the count of columns
+-- total count of columns
 SELECT count(*) as Column_Count
 FROM information_schema.columns
 WHERE table_name = 'spot';
-
 
 
 --return the datatypes of all the columns
@@ -22,4 +23,22 @@ TABLE_NAME,
 COLUMN_NAME, 
 DATA_TYPE		--returns the datatype
 FROM INFORMATION_SCHEMA.COLUMNS
+
+
+--display the artist and their corresponding songs
+select artist, song
+from dbo.spot
+
+
+--display artist in the ascending order
+select artist from dbo.spot
+order by artist asc
+
+
+--return unique artists in ascending order
+select distinct(artist)
+from dbo.spot 
+order by artist asc		--There are a total of 835 unqiue artists in this dataset
+
+
 
